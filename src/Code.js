c2f155596fdab42b6040bd02a56f64b61788aa9e
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-07-18 23:08:51 +0200

    Display menu item

--- src/Code.js
+++ src/Code.js
@@ -1,6 +1,19 @@
 // @flow
-/* global Logger */
+/* global FormApp */
+
+function displayMenu() {
+  FormApp.getUi()
+    .createAddonMenu()
+    .addItem("Make this form offline", "makeFormOffline")
+    .addToUi();
+}
+
 // eslint-disable-next-line no-unused-vars
-function myFunction(n: string) {
-  Logger.log(n * n);
+function makeFormOffline() {
+  // todo
+}
+
+// eslint-disable-next-line no-unused-vars
+function onOpen() {
+  displayMenu();
 }
