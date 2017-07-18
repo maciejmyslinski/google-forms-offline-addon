f5f43571612ec60b6d13491620bca253dbb59d03
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-07-18 23:52:41 +0200

    Remove eslint-plugin-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: ['prettier', 'plugin:flowtype/recommended', 'airbnb-base'],
-  parser: 'babel-eslint',
-  plugins: ['flowtype', 'prettier'],
-  rules: {
-    'prettier/prettier': ['error', { singleQuote: true }],
-  },
+  extends: ["prettier", "plugin:flowtype/recommended", "airbnb-base"],
+  parser: "babel-eslint",
+  plugins: ["flowtype", "prettier"]
 };
