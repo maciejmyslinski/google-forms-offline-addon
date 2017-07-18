b53ca0efcaee6b39bfeb7d633d9beabd75d941df
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-07-18 23:18:49 +0200

    Use prettier-eslint-cli

--- src/Code.js
+++ src/Code.js
@@ -4,13 +4,13 @@
 function displayMenu() {
   FormApp.getUi()
     .createAddonMenu()
-    .addItem("Make this form offline", "makeFormOffline")
+    .addItem('Make this form offline', 'makeFormOffline')
     .addToUi();
 }
 
 // eslint-disable-next-line no-unused-vars
 function makeFormOffline() {
-  FormApp.getUi().alert("Hello from Forms Offline Addon!");
+  FormApp.getUi().alert('Hello from Forms Offline Addon!');
 }
 
 // eslint-disable-next-line no-unused-vars
