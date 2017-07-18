9cb4ce28fc0b3bff42c40002419ae9c188482aea
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-07-18 23:23:09 +0200

    Improve precommit hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  extends: ["prettier", "plugin:flowtype/recommended", "airbnb-base"],
-  parser: "babel-eslint",
-  plugins: ["flowtype", "prettier"],
+  extends: ['prettier', 'plugin:flowtype/recommended', 'airbnb-base'],
+  parser: 'babel-eslint',
+  plugins: ['flowtype', 'prettier'],
   rules: {
-    "prettier/prettier": ["error", { singleQuote: true }]
-  }
+    'prettier/prettier': ['error', { singleQuote: true }],
+  },
 };
