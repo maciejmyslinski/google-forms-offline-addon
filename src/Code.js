f7cc52587e28bacf36943a43f49933ab824efb11
Maciej Myśliński <maciej.myslinski@netguru.pl>
2017-08-13 11:15:16 +0200

    Fix menu item function

--- src/Code.js
+++ src/Code.js
@@ -1,7 +1,14 @@
 /* global app */
 
+// this function is used by an menu item
+// eslint-disable-next-line no-unused-vars
+function makeFormOffline() {
+  const activeFormDescription = app.describeActiveForm();
+  Logger.log(activeFormDescription);
+}
+
 function onOpen() {
-  app.doOnOpen();
+  FormApp.getUi().createAddonMenu().addItem('Make this form offline', 'makeFormOffline').addToUi();
 }
 
 // eslint-disable-next-line no-unused-vars
